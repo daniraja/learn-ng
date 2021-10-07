c3a55d213ec7104bae3e8550eb7105384c5900e5
Daniel S <dani.raja@yahoo.com>
2021-10-06 22:41:08 -0400

    Asded route to EXP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
+import { ExpComponent } from './exp/exp.component';
+
+
 const routes: Routes = [
-  {path: 'registration', component: RegistrationComponent}
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'exp', component: ExpComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
