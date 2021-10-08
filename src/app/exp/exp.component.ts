c97284f27105e0d31a50dc56b2d96ef9db830bde
Jedidiah John <jedi.unicef@gmail.com>
2021-10-07 21:56:13 -0400

    exp

--- src/app/exp/exp.component.ts
+++ src/app/exp/exp.component.ts
@@ -1,51 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray, ValidatorFn } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-exp',
   templateUrl: './exp.component.html',
   styleUrls: ['./exp.component.scss']
 })
-export class ExpComponent {
-   public errorMessage =service: any;
-   showValidationError!: boolean;
-validationError: any;
-   service: any;
- ‘’;
+export class ExpComponent implements OnInit{
    
-   constructor(private errorNotificationService:
-      ErrorNotificationService) { }
-
-   public submitForm()
-{
-   this.service.sendForm()
-   .pipe(catchError((e: HttpErrorResponse)=>{
-      if (e.status === 422){
-         this.showValidationError = true;
-         this.validationError = e.error.error;
-         return of(null);
-      }
-      // TODO: Catch other errors: cf. next section
-   }))
-   .subscribe(/* TODO: Handle success */);
-}
+   updateForm: FormGroup;
+   
+   constructor(
+      private formBuilder: FormBuilder,
+      ) { }
 
    ngOnInit() {
-      this.errorNotificationService.notification.subscribe({
-         next: (notification) => {
-               this.errorMessage = notification;
-         },
-      });
+      
+         this.updateForm = this.formBuilder.group(
+            {
+               name: [null, Validators.required],
+            }, 
+            { updateOn: 'submit' }
+            );
+      
    }
  
 }
 
-
-function catchError(arg0: (e: any) => any): any {
-   throw new Error('Function not implemented.');
-}
-
-function of(arg0: null) {
-   throw new Error('Function not implemented.');
-}
 
