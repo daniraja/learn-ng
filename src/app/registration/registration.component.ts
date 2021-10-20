e81c21d0c5f49f91285a8d837955010d0ce8a86e
Daniel S <dani.raja@yahoo.com>
2021-10-20 15:10:47 -0400

    Added check for confim password

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,54 +1,77 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidatorFn,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 import { RegistrationService } from './registration.service';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
-  registration:any;
-  passmsg: string | undefined; 
+  registration: any;
+  passmsg: string | undefined;
 
-  constructor(private registrationService: RegistrationService){
+  constructor(private registrationService: RegistrationService) {}
 
+  ngOnInit() {
+    this.registration = new FormGroup(
+      {
+        firstName: new FormControl('', [
+          Validators.required,
+          Validators.maxLength(100),
+          Validators.minLength(4),
+          Validators.pattern(`^[a-zA-Z]+$`),
+        ]),
+        lastName: new FormControl('', [
+          Validators.pattern(`^[a-zA-Z]+$`),
+          Validators.required,
+          Validators.maxLength(100),
+          Validators.minLength(4),
+        ]),
+        email: new FormControl('', [
+          Validators.required,
+          Validators.pattern(`^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$`),
+        ]),
+        phone: new FormControl('', [
+          Validators.required,
+          Validators.pattern(`^((\\+1-?)|0)?[0-9]{10}$`),
+        ]),
+        password: new FormControl(null, [
+          Validators.required,
+          Validators.maxLength(100),
+          Validators.minLength(4),
+        ]),
+        confirmPassword: new FormControl(null, [Validators.required]),
+      },
+      { validators: checkPassword() }
+    );
   }
-  
-ngOnInit(){
-  this.registration = new FormGroup({
-    firstName: new FormControl('', [Validators.required, Validators.maxLength(100), Validators.minLength(4), Validators.pattern(`^[a-zA-Z]+$`)]),
-    lastName: new FormControl('', [Validators.pattern(`^[a-zA-Z]+$`), Validators.required, Validators.maxLength(100), Validators.minLength(4)]),
-    email: new FormControl('', [Validators.required, Validators.pattern(`^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$`)]),
-    phone: new FormControl('', [Validators.required, Validators.pattern(`^((\\+1-?)|0)?[0-9]{10}$`)]),
-    password: new FormControl('', [Validators.required]),
-    confirmPassword: new FormControl('', [Validators.required]),
-    
-});
-}
 
-checkPassSame() {
-  let pass = this.registration.value.password;
-  let passConf = this.registration.value.confirmPassword;
-  if(pass == passConf && this.registration.valid === true) {
-    this.passmsg = "";
-    return false;
-  }else {
-    this.passmsg = "Password did not match.";
-    return true;
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.registration.value);
+    this.registrationService
+      .addRegistration(this.registration.value)
+      .then((Response) => {
+        console.log('successfully Saved', Response);
+      })
+      .catch((Error) => {
+        console.error('failed to save', Error);
+      });
   }
 }
 
-onSubmit() {
- // TODO: Use EventEmitter with form value
-  console.warn(this.registration.value);
-  this.registrationService.addRegistration(this.registration.value).then(Response=> {
-    console.log("successfully Saved", Response)
-
-  }).catch(Error=> {
-    console.error("failed to save", Error)
-  })
+function checkPassword(): ValidatorFn {
+  return (group: AbstractControl): ValidationErrors | null => {
+    return group.get('password')?.value === group.get('confirmPassword')?.value
+      ? null
+      : { notSame: true };
+  };
 }
-}
-
-
